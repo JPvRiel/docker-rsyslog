0c1c94bb0dfd081e82f51a5ba47005ea92634732
JPvRiel <jp.vanriel@gmail.com>
2022-05-08 22:07:38 +0200

    modify config expand script to support modern include rainerscript syntax

--- usr/local/bin/rsyslog_config_expand.py
+++ usr/local/bin/rsyslog_config_expand.py
@@ -1,24 +1,31 @@
-#!/usr/bin/env python
+#!/usr/bin/env python3
 import sys
 import glob
 import re
 
-def expand_included_config(conf_file='/etc/rsyslog.conf', re_directive=r'^[\t ]*\$IncludeConfig (.*)'):
+def expand_included_config(
+        conf_file='/etc/rsyslog.conf',
+        re_directives=[
+            r'\s*\$IncludeConfig (?P<conf_file_legacy>\S+).*',
+            r'\s*include\(\s*file="(?P<conf_file_rainerscript>[^"]+)".*'
+        ]
+    ):
     with open(conf_file, 'r') as parent_file:
         n = 0
         for l in parent_file.readlines():
             n += 1
             sys.stdout.write("{:>3}: {}".format(n, l))
-            m = re.match(re_directive, l)
-            if m:
-                sys.stdout.write('##< start of include directive: ')
-                sys.stdout.write(l)
-                for f in sorted(glob.glob(m.group(1))):
-                    sys.stdout.write('##^ expanding file: {0:s}\n'.format(f))
-                    # recurse in case children config files also have $Include
-                    expand_included_config(f)
-                sys.stdout.write('##> end of expand directive: ')
-                sys.stdout.write(l)
+            for re_directive in re_directives:
+                m = re.match(re_directive, l)
+                if m:
+                    sys.stdout.write('##< start of include directive: ')
+                    sys.stdout.write(l)
+                    for f in sorted(glob.glob(m.group(1))):
+                        sys.stdout.write('##^ expanding file: {0:s}\n'.format(f))
+                        # recurse in case children config files also have $Include
+                        expand_included_config(f)
+                    sys.stdout.write('##> end of expand directive: ')
+                    sys.stdout.write(l)
 
 if __name__ == "__main__":
     if len(sys.argv) == 1:
