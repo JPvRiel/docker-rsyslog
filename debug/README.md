c3913b2ea6561abe1e6623ee4d298fcd3d672772
JPvRiel <jp.vanriel@gmail.com>
2022-08-22 11:54:41 +0200

    Simplify imkafka bug test script output

--- debug/README.md
+++ debug/README.md
@@ -4,6 +4,6 @@ Place to demo issues or limitations containerising rsyslog.
 
 Issues:
 
-- `imkafka_load`: [RHEL/AlmaLinux 8 error creating kafka handle and thread #4966](https://github.com/rsyslog/rsyslog/issues/4966)
+- `imkafka_load`: [RHEL 8 OS family error creating kafka handle and thread #4966](https://github.com/rsyslog/rsyslog/issues/4966)
   - Demo's that the same RHEL 8 kafka issue is not AlmaLinux specific, but also and issue with RedHat and Rocky Linux.
-  - Also demo Ubuntu does not have this issue.
+  - Ubuntu 22.04 does not have this issue.
--- debug/imkafka_load/test.sh
+++ debug/imkafka_load/test.sh
@@ -2,15 +2,18 @@
 
 set -e
 
-for i in rocky8 ubuntu22; do
-    echo "### Build and start container for $i"
-    docker build -f "Dockerfile-$i" -t "jpvriel/rsyslog-kafka-debug:$i" .
-    docker run -d --rm --name "rsyslog-kafka-debug-$i" "jpvriel/rsyslog-kafka-debug:$i"
-done
+image_list='ubuntu22 rocky8 alma8 redhat-ubi8 redhat-ubi7'
 
-for i in rocky8 ubuntu22; do
-    echo "### Check for bug in docker logs for $i"
-    docker logs "rsyslog-kafka-debug-$i" | grep 'imkafka: error creating kafka handle: Failed to create thread: No such file or directory (2)'
+for i in $image_list; do
+    echo -e "\n### Build and start container for $i"
+    docker build -f "Dockerfile-$i" -t "jpvriel/rsyslog-kafka-debug:$i" . > /dev/null
+    docker run -d --name "rsyslog-kafka-debug-$i" "jpvriel/rsyslog-kafka-debug:$i" > /dev/null
+    echo -e "\n### Check for bug in docker logs for $i"
+    if docker logs "rsyslog-kafka-debug-$i" 2>&1 | grep -F 'imkafka: error creating kafka handle: Failed to create thread: No such file or directory (2)'; then
+        echo "*** BUG FOUND for $i! ***"
+    else
+        echo "*** no bug for $i! ***"
+    fi
     echo "### Kill $i"
-    docker kill "rsyslog-kafka-debug-$i"
+    docker kill "rsyslog-kafka-debug-$i" && sleep 1 && docker rm "rsyslog-kafka-debug-$i" > /dev/null
 done
